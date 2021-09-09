3a16f04368b5a47c4205d03e499731cb21068c56
techpilot <ngwustephen99@gmail.com>
2021-09-09 02:31:37 +0100

    Amazon clon

---
+++ firebase.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBHS0bww3KCC72hpzZhVwK1uKpJRKafw24",
+  authDomain: "fir-bb778.firebaseapp.com",
+  projectId: "fir-bb778",
+  storageBucket: "fir-bb778.appspot.com",
+  messagingSenderId: "1083254437450",
+  appId: "1:1083254437450:web:fb9b4237ebff48da4998b3",
+  measurementId: "G-MS52ZRC1CD"
+};
--- src/slices/basketSlice.js
+++ src/slices/basketSlice.js
@@ -4,12 +4,33 @@ const initialState = {
   items: [],
 };
 
+// Create a slice
 export const basketSlice = createSlice({
   name: "basket",
   initialState,
   reducers: {
-    addToBasket: (state, action) => {},
-    removeFromBasket: (state, action) => {},
+    // Actions
+    addToBasket: (state, action) => {
+      state.items = [...state.items, action.payload]
+    },
+    removeFromBasket: (state, action) => {
+      const index = state.items.findIndex(
+        basketItem => basketItem.id === action.payload.id
+      );
+
+      // make a copy of the current basket
+      let newBasket = [...state.items]
+
+      if(index >= 0) {
+      //  the item exists in the basket... remove it...
+        newBasket.splice(index, 1)
+      } else {
+        //  item doesn't exist
+        console.warn(`Cannot remove the product ${action.payload.id} as it is not in the basket`)
+      }
+
+      state.items = newBasket
+    },
   },
 });
 
@@ -17,5 +38,6 @@ export const { addToBasket, removeFromBasket } = basketSlice.actions;
 
 // Selectors - This is how we pull information from the Global store slice
 export const selectItems = (state) => state.basket.items;
+export const selectTotal = (state) => state.basket.items.reduce((total, item) => total + item.price, 0)
 
 export default basketSlice.reducer;
